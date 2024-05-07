649bf3e274c74bd495dbdb6d48c4ee816d1abf06
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-05-07 19:23:26 +0000

    Return 0 if num is 1

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,7 +5,7 @@ This program holds two functions.
 
 is_prime: Determines if a given number is prime.
 
-get_prime_factors: Calculates the prime factors of a given number.
+minOperations: Calculates the prime factors of a given number.
 """
 
 
@@ -29,26 +29,26 @@ def is_prime(num):
 
 def minOperations(num):
     """
-    Calculates the minimum operations of a given number.
+    Calculates the minimum operations required to find the n chars using the
+    prime factors of a given number (n).
 
-    :param num: The number to calculate the minimum operations of.
+    :param num: The number to calculate the prime factors of.
     :type num: int
-    :return: Sum of prime factors.
+    :return: A sum of prime factors.
     :rtype: int
     """
     # Initialize variables for the function
     number_to_get_factors = num
     prime_factors = []
 
-    # if n char is impossible to achieve, return 0
-    if num <= 0:
+    # if num is impossible return 0
+    if num <= 1:
         return 0
 
-    # If the number is prime, return a list with 1 and the number itself
+    # If the number is prime, return a list with the number itself
     check = is_prime(num)
     if check == 0:
-        prime_factors.append(num)
-        return prime_factors
+        return num
 
     # If the number is not prime, iterate through numbers from 2 to the
     # given number (inclusive)
